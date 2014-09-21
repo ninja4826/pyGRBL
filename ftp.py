c5d042f361c1a2d30e96591690f51eaf3d437f59
ninja4826 <hueske.russ690@gmail.com>
2014-09-21 09:46:07 -0500

    stuff

--- ftp.py
+++ ftp.py
@@ -7,32 +7,32 @@ file = 'asdf.txt'
 file_exists = False
 r = StringIO()
 
-		try:
-			ftp = ftplib.FTP(server)
-			ftp.login("russ")
-		except Exception,e:
-			print e
-		else:
-			filelist = []
-			ftp.retrlines('LIST', filelist.append)
-		for f in filelist:
-			if file in f:
-				file_exists = True
-		if file_exists:
-			ftp.retrbinary('RETR %s' % file, r.write)
-			r_string = r.getvalue()
-			arr = [s.strip() for s in r_string.splitlines()]
-			total = 0
-			for l in arr:
-				total += 1
+try:
+	ftp = ftplib.FTP(server)
+	ftp.login("russ")
+except Exception,e:
+	print e
+else:
+	filelist = []
+	ftp.retrlines('LIST', filelist.append)
+for f in filelist:
+	if file in f:
+		file_exists = True
+if file_exists:
+	ftp.retrbinary('RETR %s' % file, r.write)
+	r_string = r.getvalue()
+	arr = [s.strip() for s in r_string.splitlines()]
+	total = 0
+	for l in arr:
+		total += 1
 
-			i = 0
-			
-			for l in arr:
-				i += 1
-				print str((i/total)*100)
-				print l
-		else:
-			print ':('
-			
-		r.close()
+	i = 0
+	
+	for l in arr:
+		i += 1
+		print str((i/total)*100)
+		print l
+else:
+	print ':('
+	
+r.close()
